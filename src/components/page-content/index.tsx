8b27f235f862c742237983059a166288d5e4c48c
gmoinbong <vladosos.scherba@gmail.com>
2023-05-27 18:26:51 +0300

    order

--- src/components/page-content/index.tsx
+++ src/components/page-content/index.tsx
@@ -8,22 +8,30 @@ import zavod3 from '../../assets/zavod3.jpg'
 import zavod4 from '../../assets/zavod4.jpg'
 
 import './Content.css'
+import OrderForm from '../order-form';
+import OrderSelect from '../order-select';
 
 export const Content = () => {
+  const handleSubmit = (data: FormData) => {
+    console.log(data, "data")
+  };
+
   return (
     <>
       <Container className='container' maxWidth='false'>
         <h1>Свіже та натуральне молоко, доставлене прямо до вас</h1>
         <div className='main-container'>
+          <p className='intro'>Ласкаво просимо на наш завод з виробництва молока.
+            Ми пишаємося тим, що пропонуємо нашим клієнтам найсвіжіше і натуральне молоко, багате поживними речовинами.</p>
           <Slider >
             <SliderItem> <img className='sliderImage' src={zavod1} alt="" /></SliderItem>
             <SliderItem> <img className='sliderImage' src={zavod2} alt="" /></SliderItem>
             <SliderItem> <img className='sliderImage' src={zavod3} alt="" /></SliderItem>
             <SliderItem> <img className='sliderImage' src={zavod4} alt="" /></SliderItem>
           </Slider>
-          <p className='intro'>Ласкаво просимо на наш завод з виробництва молока.
-            Ми пишаємося тим, що пропонуємо нашим клієнтам найсвіжіше і натуральне молоко, багате поживними речовинами.</p>
         </div>
+        <OrderForm onSubmit={handleSubmit} />
+        <OrderSelect />
       </Container>
     </>
   )
